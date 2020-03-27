583f9c0b68743a6f93c7da78165735c41c942a77
Miguel <miguel.g282@gmail.com>
2020-03-27 15:44:17 -0700

    Commit for adding navigator functionality to App.js, and adding SearchScreen and SearchBar components.

---
+++ food/src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchBar = () => {
+    return (
+        <View>
+            <Text>Hello from Search Screen</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default SearchBar;
