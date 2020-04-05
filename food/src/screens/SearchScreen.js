dc69df6f6333231f31fac996276f87a2b48ce13c
Miguel <miguel.g282@gmail.com>
2020-04-05 12:59:55 -0700

    Commit for adding yelp search API with success on results

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -5,16 +5,29 @@ import yelp from '../api/yelp';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [bizResults, setBizResults] = useState([]);
+
+    const searchApi = async () => {
+      const response = await yelp.get('/search', {
+          params: {
+              limit: 50,
+              term,
+              location: 'Long Beach'
+          }
+      });
+      setBizResults(response.data.businesses);
+        
+    }
 
     return (
         <View>
             <SearchBar 
             term={term} 
-            onTermChange={(newTerm) => setTerm(newTerm)}
-            onTermSubmit={() =>console.log('onTermSubmit') } 
+            onTermChange={setTerm} // same as: {(newTerm) => setTerm()}
+            onTermSubmit={searchApi} // same as: {() => searchApi()}
             />
             <Text>Hello from Search Screen</Text>
-            <Text>{term}</Text>
+            <Text>We have found {bizResults.length} results</Text>
         </View>
     )
 };
